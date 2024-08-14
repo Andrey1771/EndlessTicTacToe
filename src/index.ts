8215c6641ef79242ee26dc0732242471d81c9f0c
Andrey Khokhlov <54952700+Andrey1771@users.noreply.github.com>
2024-08-14 16:01:10 +0300

    feat(ui): финальный фикс таблицы 3 часть
    Исправил часть ошибок работы поля
    
    BREAKING CHANGE: изменения в index.ts

--- src/index.ts
+++ src/index.ts
@@ -23,8 +23,6 @@ function createCell(row: number, col: number, cellKey: string, visibleCells: Map
         cell.dataset.row = row.toString();
         cell.dataset.col = col.toString();
         cell.classList.add(cellKey);
-        //gameBoard.appendChild(cell);
-        //gameBoard.insertAdjacentElement(, cell);
         insertCellInGameBoard(row, col, cell);
         visibleCells.set(cellKey, "");
         return;
@@ -35,9 +33,7 @@ function createCell(row: number, col: number, cellKey: string, visibleCells: Map
         cell.dataset.col = col.toString();
         const value = crossValue ? 'x' : 'o';
         cell.classList.add(value);
-        //cell.classList.add(cellKey);
         cell.textContent = value;
-        //gameBoard.appendChild(cell);
         insertCellInGameBoard(row, col, cell);
         visibleCells.set(cellKey, value);
         return;
@@ -46,10 +42,14 @@ function createCell(row: number, col: number, cellKey: string, visibleCells: Map
     throw new Error("Почему");
 }
 
-function insertCellInGameBoard(row: number, col: number, cell: HTMLDivElement): void {
-    const nearestCell = getNearestCell(row, col);
+export function insertCellInGameBoard(row: number, col: number, cell: HTMLDivElement): void {
+    // Получение всех ячеек
+    let cells = gameBoard.querySelectorAll('.cell');
+
+    const nearestCell = getNearestCell(row, col, cells);
     if (!nearestCell) {
-        gameBoard.appendChild(cell);
+        gameBoard.prepend(cell);
+        console.log("gameBoard.prepend(cell)");
         return;
     }
 
@@ -66,9 +66,19 @@ function insertCellInGameBoard(row: number, col: number, cell: HTMLDivElement): 
         throw new Error(`Ячейка (${row}:${col}) с такими координатами уже существует.`);
     }
 
+    if (row < nearestRow && col === nearestColumn) {
+        const firstCellOfRow = getFirstCellOfRow(nearestRow, cells);
+        if (firstCellOfRow === null) {
+            throw new Error("Первая ячейка строки не была найдена");
+        }
+
+        firstCellOfRow.insertAdjacentElement("beforebegin", cell);
+        return;
+    }
+
     // Если новая ячейка должна быть вставлена перед ближайшей ячейкой
-    if ((row < nearestRow && col === nearestColumn) || (row === nearestRow && col < nearestColumn)) {
-        gameBoard.insertBefore(cell, nearestCell);
+    if ((row === nearestRow && col < nearestColumn)) {
+        nearestCell.insertAdjacentElement("beforebegin", cell);
         return;
     }
 
@@ -82,56 +92,50 @@ function insertCellInGameBoard(row: number, col: number, cell: HTMLDivElement): 
     if (row !== nearestRow && col !== nearestColumn) {
         const nextSibling = nearestCell.nextSibling;
         if (nextSibling) {
-            gameBoard.insertBefore(cell, nextSibling);
+            gameBoard.insertAdjacentElement("afterbegin", cell);
         } else {
-            gameBoard.appendChild(cell);
+            console.log(`Не найден nextSibling для (${row}, ${col})`);
         }
         return;
     }
 
     // Если случай, который не должен возникать
-    throw new Error("Как я тут оказался?");
+    throw new Error("Получены не поддерживаемые условия вставки");
 }
 
-
-/*
-position – A string representing the position relative to the element. Must be one of the following strings:
-"beforebegin"
-Before the element. Only valid if the element is in the DOM tree and has a parent element.
-"afterbegin"
-Just inside the element, before its first child.
-"beforeend"
-Just inside the element, after its last child.
-"afterend"
-After the element. Only valid if the element is in the DOM tree and has a parent element.
- */
-
-// Function to highlight a path from start to end cell
-function getNearestCell(currentRow: number, currentCol: number): Element | null {
-    // Получение всех ячеек
-    let cells = gameBoard.querySelectorAll('.cell');
-
+function getNearestCell(currentRow: number, currentCol: number, cells: NodeListOf<Element>): Element | null {
     // Переменные для хранения ближайшей ячейки и минимального расстояния
     let closestCell = null;
-    let minRowDistance = Infinity;
-    let minColDistance = Infinity;
+
+    let minRowDistance: number | null = null;
+    let minColDistance: number | null = null;
+
+    if (cells.length > 0) {
+        const firstRowAndCol = getRowAndColFromDOM(cells[0]);
+        let firstCellRow = firstRowAndCol.row;
+        let firstCellCol = firstRowAndCol.col;
+
+        minRowDistance = Math.abs(currentRow - firstCellRow);
+        minColDistance = Math.abs(currentCol - firstCellCol);
+    } else if (cells.length === 1) {
+        return cells[0];
+    } else {
+        return null;
+    }
 
     // Поиск ближайшей ячейки
     cells.forEach(cell => {
-        const dataRow = cell.getAttribute('data-row');
-        const dataColumn = cell.getAttribute('data-col');
-        if (dataRow === null || dataColumn === null) {
-            throw new Error("при попытке getNearestCell выявлено dataRow === null || dataColumn === null");
-        }
+        const rowAndCol = getRowAndColFromDOM(cell);
 
-        let cellRow = parseInt(dataRow);
-        let cellCol = parseInt(dataColumn);
+        let cellRow = rowAndCol.row;
+        let cellCol = rowAndCol.col;
 
         // Расчет расстояния по строке и колонке
         let rowDistance = Math.abs(currentRow - cellRow);
         let colDistance = Math.abs(currentCol - cellCol);
 
         // Сначала проверяем строку, затем колонку
+        // @ts-ignore
         if (rowDistance < minRowDistance || (rowDistance === minRowDistance && colDistance < minColDistance)) {
             minRowDistance = rowDistance;
             minColDistance = colDistance;
@@ -140,6 +144,35 @@ function getNearestCell(currentRow: number, currentCol: number): Element | null 
     });
 
     return closestCell;
+}
+
+function getFirstCellOfRow(currentRow: number, cells: NodeListOf<Element>): Element | null {
+    let cellsOfRow: Element[] = [];
+    cells.forEach(cell => {
+        const rowAndCol = getRowAndColFromDOM(cell);
+        if (rowAndCol.row === currentRow) {
+            cellsOfRow.push(cell);
+        }
+    });
+
+    return cellsOfRow.reduce((prev, curr) => {
+        const prevCol = getRowAndColFromDOM(prev).col;
+        const currCol = getRowAndColFromDOM(prev).col;
+
+        return currCol < prevCol ? curr : prev;
+    });
+}
+
+function getRowAndColFromDOM(cell: Element): {row: number, col: number} {
+    const dataRow = cell.getAttribute('data-row');
+    const dataColumn = cell.getAttribute('data-col');
+    if (dataRow === null || dataColumn === null) {
+        throw new Error("при попытке getNearestCell выявлено dataRow === null || dataColumn === null");
+    }
+
+    let cellRow = parseInt(dataRow);
+    let cellCol = parseInt(dataColumn);
+    return { row: cellRow, col: cellCol };
 }
 
 function updateBoard(): void {
@@ -153,23 +186,11 @@ function updateBoard(): void {
     const minVisibleCol = Math.floor(containerScrollLeft / cellSize) - loadDistance;
     const maxVisibleCol = Math.ceil((containerScrollLeft + containerWidth) / cellSize) + loadDistance;
 
-
-    for (let col = minVisibleCol; col <= maxVisibleCol; col++) {
-        for (let row = minVisibleRow; row <= maxVisibleRow; row++) {
-            const cellKey = `${row},${col}`;
-            if (!visibleCells.has(cellKey)) {
-                // Если ячейка новая, добавляем её
-                createCell(row, col, cellKey, visibleCells);
-            }
-        }
-    }
-
     // Удаляем не видимые ячейки
     visibleCells.forEach((value, cellKey) => {
         const [rowStr, colStr] = cellKey.split(',');
         const row = parseInt(rowStr, 10);
         const col = parseInt(colStr, 10);
-
         if (row < minVisibleRow || row > maxVisibleRow || col < minVisibleCol || col > maxVisibleCol) {
             const cellToRemove = gameBoard.querySelector(`.cell[data-row="${row}"][data-col="${col}"]`);
             if (cellToRemove) {
@@ -181,14 +202,24 @@ function updateBoard(): void {
         }
     });
 
+    for (let row = minVisibleRow; row <= maxVisibleRow; row++) {
+        for (let col = maxVisibleCol; col >= minVisibleCol; col--) {
+            const cellKey = `${row},${col}`;
+            if (!visibleCells.has(cellKey)) {
+                // Если ячейка новая, добавляем её
+                createCell(row, col, cellKey, visibleCells);
+            }
+        }
+    }
+
     // Обновляем стили сетки
     gameBoard.style.gridTemplateColumns = `repeat(${maxVisibleCol - minVisibleCol + 1}, ${cellSize}px)`;
     gameBoard.style.gridTemplateRows = `repeat(${maxVisibleRow - minVisibleRow + 1}, ${cellSize}px)`;
 
-    const horizontalSizeOfRemovedColumns = Math.abs(Math.abs(minVisibleCol) - loadDistance) * cellSize;
-    const verticalSizeOfRemovedRows = Math.abs(Math.abs(minVisibleRow) - loadDistance) * cellSize;
-    gameBoard.style.paddingLeft = `${horizontalSizeOfRemovedColumns}px`;
-    gameBoard.style.paddingTop = `${verticalSizeOfRemovedRows}px`;
+    const horizontalSizeOfRemovedColumns = Math.abs(-minVisibleCol - loadDistance) * cellSize;
+    const verticalSizeOfRemovedRows = Math.abs(-minVisibleRow - loadDistance) * cellSize;
+    gameBoard.style.marginLeft = `${horizontalSizeOfRemovedColumns}px`;
+    gameBoard.style.marginTop = `${verticalSizeOfRemovedRows}px`;
 }
 
 function onScroll(): void {
